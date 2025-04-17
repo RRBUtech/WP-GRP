d798aa818647d35c851d8c8cf87a63cf8477af00
freescode <fostermerrick7@gmail.com>
2025-04-16 22:40:00 -0500

    Update dashboard.js

--- codes/scripts/dashboard.js
+++ codes/scripts/dashboard.js
@@ -1,3 +1,14 @@
+/*QUESTION 6 
+6.	Additional Functionality:
+      a.	ShowUserFrequency() – Show’s user requency based on Gender and Age Group:
+      i.	show how many registered users fall under specific gender categories (e.g. Male, Female, Other)
+      ii.	show how many registered users fall under different age groups (e.g., 18-25, 26-35, 36-50, 50+).
+      iii.	Display this data on a dashboard or a separate page. 
+b.	ShowInvoices() - displays all invoices and allow the visitor to search for any of the invoices (using trn) stored in AllInvoices from localStorage using console.log().
+c.	GetUserInvoices() – displays all the invoices for a user based on trn stored in the localStorage key called, RegisterData. 
+
+
+*/
 // Check if user is logged in and is admin
 function checkAdminAccess() {
   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
